e272b312823468a8b9a096248c79a3fe81fa520f
Michal Wojcik <wojcik@warpdynamics.co.uk>
2025-09-21 18:12:52 +0200

    fix workflow

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE -DCMAKE_C_COMPILER=/usr/bin/gcc
 cmake --build build --config Debug --target all -j 8 || exit
 build/tests/tests
